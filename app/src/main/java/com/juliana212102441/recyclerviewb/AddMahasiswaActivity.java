b1aa13efcc0b4530675ef0a1c749c242583501e5
feniaster <92568120+JulianaSuwandi@users.noreply.github.com>
2023-06-13 10:02:08 +0700

    Initial commit

---
+++ app/src/main/java/com/juliana212102441/recyclerviewb/AddMahasiswaActivity.java
@@ -0,0 +1,13 @@
+package com.juliana212102441.recyclerviewb;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddMahasiswaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_mahasiswa);
+    }
+}
